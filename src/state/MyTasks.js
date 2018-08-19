823ed4298d71dc029b95b4dd07d87a325cb6e949
JarekZ92 <z.jarek92@gmail.com>
2018-08-19 20:12:16 +0200

    added more actions

--- src/state/MyTasks.js
+++ src/state/MyTasks.js
@@ -1,15 +1,22 @@
 const ADD_TASK = "tasks/ADD_TASK";
 const SET_TASK = "tasks/SET_TASK";
 const REMOVE_TASK = "tasks/REMOVE_TASK";
+const HANDLE_CHANGE = 'tasks/HANDLE_CHANGE'
+const TASKS_STARTED_LOADING = 'tasks/TASKS_STARTED_LOADING'
+const TASKS_STOPPED_LOADING = 'tasks/TASKS_STOPPED_LOADING'
+
 
 export const addTaskAction = value => ({ type: ADD_TASK, value })
 export const setTasksAction = data => ({ type: SET_TASK, data })
 export const removeTasksAction = () => ({ type: REMOVE_TASK })
-
+export const handleChangeAction = (event) => ({ type: HANDLE_CHANGE, text: event.target.value })
+export const tasksStartedLoadingAction = () => ({ type: TASKS_STARTED_LOADING })
+export const tasksStoppedLoadingAction = () => ({ type: TASKS_STOPPED_LOADING })
 
 const initialState ={
     tasks: [],
-    text: ''
+    text: '',
+    isTasksAreLoading: false
 }
 
 export default (state = initialState, action) => {
