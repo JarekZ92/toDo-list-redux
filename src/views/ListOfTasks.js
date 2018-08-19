717d2dbf1a93d398ce6a77d921bbaa322ea35b24
JarekZ92 <z.jarek92@gmail.com>
2018-08-19 23:45:35 +0200

    added RaiseButton and props into TextField

--- src/views/ListOfTasks.js
+++ src/views/ListOfTasks.js
@@ -4,13 +4,24 @@ import TextField from "material-ui/TextField"
 import RaisedButton from "material-ui/RaisedButton"
 import Task from "./Task"
 import { ListItem } from "material-ui/List"
+import {addTaskAction, handleChangeAction} from '../state/tasks'
+
 
 const listOfTasks = (props) => (
   <div>
     <div>
       <TextField 
-      
+      onChange={(value) => props._handleChangeAction(value)}
+      placeholder={'Enter new task here'}
+      type='text'
+      value = {props._task}
+      fullWidth={true}
       />
+      <RaisedButton 
+            onClick={props._addTaskAction} 
+            label="Add task" 
+            primary={true}
+            fullWidth={true} />
     </div>
   </div>
 )
